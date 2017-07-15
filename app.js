e57f3e13c060a31d757d3409512609aa0f6fed09
YogeshPrasanna <yogesh15031993@gmail.com>
2017-07-15 16:38:43 +0530

    add card layout to display data , css changes

--- app.js
+++ app.js
@@ -16,22 +16,42 @@ $(document).ready(function() {
             dataType: 'jsonp',
             success: function(data) {
 
-                var Results = [];
                 var Titles = data[1].map((elem) => elem);
                 var Details = data[2].map((elem) => elem);
                 var Links = data[3].map((elem) => elem);
+                var Images = []
 
-                Titles.map(function(elem) {
-                    return Results.push(elem)
-                });
+                //Fetch images of that wiki page - currently not working
+                //      Titles.map(function(elem){
+                //      	$.ajax({
+                //     method: "GET",
+                //     url: "https://en.wikipedia.org/w/api.php?action=query&titles="+ elem +"&prop=pageimages&format=json&pithumbsize=100",
+                //     data: {
+                //         format: 'json'
+                //     },
+                //     dataType: 'jsonp',
+                //     success: function(img){
+                //     	console.log(Object.values(img.query.pages)[0])
+                //     	//$('.card-img-top').html(Object.values(img.query.pages)[0].thumbnail.source);
+                //     	if(Object.values(img.query.pages)[0].hasOwnProperty('thumbnail')){
+                //     		Object.values(img.query.pages)[0].thumbnail.source !== "" ? Images.push(Object.values(img.query.pages)[0].thumbnail.source) : Images.push("");
+                //     	}else{
+                //     		Images.push("");
+                //     	}
+                //     },
+                //     error: function(){
+                //     	console.log(error);
+                //     }
+                // })
+                //      })
 
-                for (var i = 0; i <= data[1].length; i++) {
-                    $('#result-section').append('<div class="card" style="width: 50rem;display:inline-block">' +
+                for (var i = 0; i < data[1].length; i++) {
+                    $('#result-section').append('<div class="col-sm-4 card animate-flicker" style="display:inline-block">' +
                         '<div class="card-img-top"></div>' +
                         '<div class="card-block">' +
                         '<h4 class="card-title">' + Titles[i] + '</h4>' +
                         '<p class="card-text">' + Details[i] + '</p>' +
-                        '<a href= ' + Links[i] + ' class="btn btn-primary">Go somewhere</a>' +
+                        '<a href= ' + Links[i] + ' class="btn btn-primary link-btn text-center" target="_blank">Checkout the wikipedia page</a>' +
                         '</div>' +
                         '</div>');
                 }
