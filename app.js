f8fa0b4b698e0c9ef97ace294d42fcbe3e153205
YogeshPrasanna <yogesh15031993@gmail.com>
2017-07-17 22:06:27 +0530

    fetch and add images from wiki api

--- app.js
+++ app.js
@@ -22,41 +22,44 @@ $(document).ready(function() {
                 var Images = []
 
                 //Fetch images of that wiki page - currently not working
-                //      Titles.map(function(elem){
-                //      	$.ajax({
-                //     method: "GET",
-                //     url: "https://en.wikipedia.org/w/api.php?action=query&titles="+ elem +"&prop=pageimages&format=json&pithumbsize=100",
-                //     data: {
-                //         format: 'json'
-                //     },
-                //     dataType: 'jsonp',
-                //     success: function(img){
-                //     	console.log(Object.values(img.query.pages)[0])
-                //     	//$('.card-img-top').html(Object.values(img.query.pages)[0].thumbnail.source);
-                //     	if(Object.values(img.query.pages)[0].hasOwnProperty('thumbnail')){
-                //     		Object.values(img.query.pages)[0].thumbnail.source !== "" ? Images.push(Object.values(img.query.pages)[0].thumbnail.source) : Images.push("");
-                //     	}else{
-                //     		Images.push("");
-                //     	}
-                //     },
-                //     error: function(){
-                //     	console.log(error);
-                //     }
-                // })
-                //      })
+                Titles.map(function(elem) {
+                    $.ajax({
+                        method: "GET",
+                        url: "https://en.wikipedia.org/w/api.php?action=query&titles=" + elem + "&prop=pageimages&format=json&pithumbsize=100",
+                        data: {
+                            format: 'json'
+                        },
+                        dataType: 'jsonp'
+                    }).done(function(img) {
+                        //console.log(Object.values(img.query.pages)[0])
+                        //$('.card-img-top').html(Object.values(img.query.pages)[0].thumbnail.source);
+                        if (Object.values(img.query.pages)[0].hasOwnProperty('thumbnail')) {
+                            Object.values(img.query.pages)[0].thumbnail.source !== "" ? Images.push(Object.values(img.query.pages)[0].thumbnail.source) : Images.push("");
+                        } else {
+                            Images.push("http://vollrath.com/ClientCss/images/VollrathImages/No_Image_Available.jpg");
+                        }
+                    }).fail(function() {
+                        // handle request failures
+                    });
+                })
 
-                for (var i = 0; i < data[1].length; i++) {
-                    $('#result-section').append('<div class="col-sm-4 card animate-flicker" style="display:inline-block">' +
-                        '<div class="card-img-top"></div>' +
-                        '<div class="card-block">' +
-                        '<h4 class="card-title">' + Titles[i] + '</h4>' +
-                        '<p class="card-text">' + Details[i] + '</p>' +
-                        '<a href= ' + Links[i] + ' class="btn btn-primary link-btn text-center" target="_blank">Checkout the wikipedia page</a>' +
-                        '</div>' +
-                        '</div>');
-                }
-
-                console.log(data);
+                //bad practice - need to find a better solution to display images
+                //under slow networks it may fail
+                setTimeout(function() {
+                    for (var i = 0; i < data[1].length; i++) {
+                        if (Images[i] !== undefined) {
+                            $('#result-section').append('<div class="col-sm-4 card animate-flicker" style="display:inline-block">' +
+                                '<img class="card-img-top" src="' + Images[i] + '" width="200" height="100"></img>' +
+                                '<div class="card-block">' +
+                                '<h4 class="card-title">' + Titles[i] + '</h4>' +
+                                '<p class="card-text">' + Details[i] + '</p>' +
+                                '<a href= ' + Links[i] + ' class="btn btn-primary link-btn text-center" target="_blank">Checkout the wikipedia page</a>' +
+                                '</div>' +
+                                '</div>');
+                        }
+                    }
+                }, 1000);
+                //console.log(data);
             },
             error: function() {
                 alert("FAIL");
